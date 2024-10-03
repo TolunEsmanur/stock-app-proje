6837c6517df03ec92f8cacd3643a38c7df305bf1
TolunEsmanur <160126791+TolunEsmanur@users.noreply.github.com>
2024-10-03 23:32:09 +0300

    Routes

--- stock-app/src/components/MenuListItems.jsx
+++ stock-app/src/components/MenuListItems.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -8,31 +6,58 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import AppsIcon from '@mui/icons-material/Apps';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney"
+import InventoryIcon from "@mui/icons-material/Inventory"
+import StoreIcon from "@mui/icons-material/Store"
+import StarsIcon from "@mui/icons-material/Stars"
+import { useNavigate } from 'react-router-dom';
   
 
 const MenuListItems = () => {
-
+    const navigate = useNavigate()
     const icons =[
     {
         title:"Dashboard",
         icon:<AppsIcon/>,
-        path:"/",
+        path:"/stock",
     },
 
     {
         title:"Purchases",
         icon:<ShoppingBasketIcon/>,
-        path:"/purchases",
+        path:"/stock/purchases",
+    },
 
-    }
+    {
+        title: "Sales",
+        icon: <AttachMoneyIcon/>,
+        path: "/stock/sales/",
+      },
 
+    {
+        title: "Firms",
+        icon: <StoreIcon />,
+        path: "/stock/firms/",
+    },
+
+    {
+        title: "Brands",
+        icon: <StarsIcon />,
+        path: "/stock/brands/",
+    },
+
+    {
+        title: "Products",
+        icon: <InventoryIcon />,
+        path: "/stock/products/",
+    },
 
 ]
   return (
     <div>
         <List>
         {icons.map((item, index) => (
-          <ListItem key={index} disablePadding>
+          <ListItem key={index} disablePadding onClick={()=>navigate(item.path)}>
             <ListItemButton>
               <ListItemIcon>
                 {item.icon}
