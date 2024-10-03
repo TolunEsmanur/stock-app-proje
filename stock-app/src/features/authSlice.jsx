d3e1716d51638aed54fc552a29f78bd077b33875
TolunEsmanur <160126791+TolunEsmanur@users.noreply.github.com>
2024-10-03 21:28:18 +0300

    dashboard

---
+++ stock-app/src/features/authSlice.jsx
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const authSlice = createSlice({
+  name: "auth",
+
+  initialState: {
+    token:"",
+    username:"",
+    loading:"",
+    error:""
+    
+  },
+  reducers: {
+    fetchStart:(state)=>{
+      state.loading=true
+    },
+
+    loginSuccess: (state, {payload})=>{
+      state.token = payload.token
+      state.username = payload.user.username
+      state.loading = false
+    },
+
+    registerSuccess: (state, {payload})=>{
+      state.token = payload.token
+      state.username = payload.data.username
+      state.loading = false
+    },
+
+    logoutSuccess: (state, {payload})=>{
+      state.token = ""
+      state.username = ""
+      state.loading = false
+    },
+
+    fetchFail:(state)=>{
+      state.loading = false
+      state.error = true
+    }
+  },
+})
+
+export const {fetchStart,fetchFail,loginSuccess,registerSuccess,logoutSuccess} = authSlice.actions
+export default authSlice.reducer
