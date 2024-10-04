6d5fe899cfe61eb9536a905fba7a18df639c2a9d
TolunEsmanur <160126791+TolunEsmanur@users.noreply.github.com>
2024-10-04 11:31:09 +0300

    getFirm

--- stock-app/src/pages/Firms.jsx
+++ stock-app/src/pages/Firms.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux';
 
-const Firms = () => {
+const Firm = () => {
+  const {token} = useSelector((state)=>state.auth)
+
+  const getFirm = async ()=>{
+    try {
+      const {data} = await axios(`${process.env.REACT_APP_BASE_URL}/firms`,{
+        headers: {Authorization:`Token ${token}`},
+      })
+        }catch (error) {
+      console.log(error);
+        }
+
+  }
+  //sayfa yüklendikten sonra sayfaları getir!
+  useEffect(() => {
+    getFirm()
+  
+   
+  }, [])
+
+  
   return (
-    <div>Firms</div>
+    <div>Firm</div>
   )
 }
 
-export default Firms
+export default Firm
