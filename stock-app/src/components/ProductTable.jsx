b2086b05b404eca6b180893f8b5910532092366d
TolunEsmanur <160126791+TolunEsmanur@users.noreply.github.com>
2024-10-09 23:16:09 +0300

    table

--- stock-app/src/components/ProductTable.jsx
+++ stock-app/src/components/ProductTable.jsx
@@ -4,53 +4,73 @@ import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
 import DeleteIcon from "@mui/icons-material/Delete"
 import { useSelector } from 'react-redux';
 
-const columns = [
-  { field: '_id', headerName: '#', width: 90 },
-  {
-    field: 'categoryId',
-    headerName: 'Categories',
-    width: 150,
-    valueGetter: (value)=>value?.name,
-    editable: true,
-  },
-  {
-    field: 'brandId',
-    headerName: 'Brands',
-    width: 150,
-    valueGetter: (value)=>value?.name,
-    editable: true,
-  },
-  {
-    field: 'name',
-    headerName: 'Name',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'quantity',
-    headerName: 'Stock',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: "actions",
-    headerName:'Operations',
-    type: "actions",
-    
-    getActions: (param) => [
-        <GridActionsCellItem icon={ <DeleteIcon/>} label="Delete"/>,
-        
-       ]
-      },
-    ];
 
 const getRowId = (row)=>row._id
 
 export default function ProductTable() {
 
   const {products} = useSelector((state)=>state.stock)
+  
+  const columns = [
+    { field: "_id", headerName: "#", flex: 1.2, minWidth: 140 },
+    {
+      field: "categoryId",
+      headerName: "Categories",
+      flex: 1,
+      minWidth: 140,
+      align: "center",
+      headerAlign: "center",
+      // valueGetter: (value, row) => {
+      //   console.log("ROW:", row, "VALUE:", value)
+      //   //   return row.categoryId.name
+      //   return value.name
+      // },
+      valueGetter: (value) => value?.name,
+    },
+    {
+      field: "brandId",
+      headerName: "Brands",
+      flex: 1,
+      minWidth: 140,
+      align: "center",
+      headerAlign: "center",
+      valueGetter: (value) => value?.name,
+      width: 150,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      type: "number",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      minWidth: 140,
+    },
+    {
+      field: "quantity",
+      headerName: "Stock",
+      type: "number",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      minWidth: 90,
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Operations",
+      getActions: (props) => {
+        // console.log(props)
+        return [
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+           
+            label="Delete"
+          />,
+        ]
+      },
+    },
+  ]
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
