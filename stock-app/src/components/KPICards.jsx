c04ce4e5f66b78d973e530980a6fd3c12718c403
TolunEsmanur <160126791+TolunEsmanur@users.noreply.github.com>
2024-10-12 14:03:51 +0300

    avatar,box

--- stock-app/src/components/KPICards.jsx
+++ stock-app/src/components/KPICards.jsx
@@ -5,32 +5,37 @@ import PaymentsIcon from '@mui/icons-material/Payments';
 import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import Avatar from '@mui/material/Avatar';
+import { deepPurple, pink, orange} from '@mui/material/colors';
+import { Box, Typography } from '@mui/material';
 
 const KPICards = () => {
   const cardData=[{
     id:1,
-    icon:<MonetizationOnIcon/>,
+    icon:<MonetizationOnIcon sx={{fontSize:"2rem"}}/>,
     title:"sales",
-    bgColor:"red",
-    color:"darkred"
+    bgColor: deepPurple[100],
+    color: deepPurple[500],
+    amount: "$12345"
 
   },
 
   {
     id:2,
-    icon:<ShoppingBasketIcon/>,
+    icon:<ShoppingBasketIcon sx={{fontSize:"2rem"}}/>,
     title:"profit",
-    bgColor:"red",
-    color:"darkred"
+    bgColor:pink[100],
+    color: pink[900],
+    amount: "$12345"
 
   },
 
   {
     id:3,
-    icon:<PaymentsIcon/>,
+    icon:<PaymentsIcon sx={{fontSize:"2rem"}}/>,
     title:"purchases",
-    bgColor:"red",
-    color:"darkred"
+    bgColor: orange[100],
+    color: orange[900],
+    amount: "$12345"
 
   },
 
@@ -38,8 +43,12 @@ const KPICards = () => {
   return (
   <Stack>
     {cardData.map((item)=>(
-    <Paper elevation={5}> 
-    <Avatar sx={{ bgcolor: "red" }}>{item.icon}</Avatar>
+    <Paper elevation={5} sx={{display:"flex"}}> 
+    <Avatar sx={{width:60, height:60, backgroundColor:item.bgColor, color:item.color }}>{item.icon}</Avatar>
+    <Box>
+      <Typography>{item.title}</Typography>
+      <Typography>{item.amount}</Typography>
+    </Box>
     </Paper>
     ))}
   </Stack>
